fe0757e63bdc539606ae353aa7ec868fe5c8d656
Ali Saleh <alisaleh06@outlook.com.au>
2025-06-29 09:21:35 +1000

    Ebook update

--- app/free-ebook/page.jsx
+++ app/free-ebook/page.jsx
@@ -3,22 +3,23 @@ import EbookForm from "@/components/sections/EbookForm";
 export default function Contact() {
     return (
         <div className="container mx-auto py-12 px-4 md:px-6 lg:py-24">
-            <div className="mx-auto">
+            <div className="mx-auto max-w-4xl">
                 <div className="text-center mb-12">
                     <h3 className="text-2xl md:text-3xl font-bold tracking-tight mb-4">
                         Free Guide
                     </h3>
                     <h1 className="text-4xl md:text-5xl font-bold tracking-tight mb-12">
-                        4 Simple Steps To Quickly Attract More Clients For Your
-                        Business With Meta Ads
+                        A Simple Checklist To Getting More Clients Using Google
+                        Ads
                     </h1>
                     <p className="text-xl text-muted-foreground max-w-2xl mx-auto">
-                        Google Ads are the quickest and most cost-effective way
-                        to attract the perfect clients for your business.
+                        Google Ads work if you get the basics right.
                         <br /> <br />
-                        In this free guide we will tell you how to do exactly
-                        that. No jargon or technical mumbling, just an easy to
-                        follow step-by-step overview of exactly what to do.
+                        It puts you right in front of people who are{" "}
+                        <strong>already</strong> searching for what you do.
+                        <br /> <br />
+                        This quick guide helps you start using Google Ads the
+                        right way, without wasting money or hiring an agency.
                     </p>
                 </div>
 
