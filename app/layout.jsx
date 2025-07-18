5ec58a3d2a7c67980b615268f123e8ee77322c16
Ali Saleh <alisaleh06@outlook.com.au>
2025-07-18 12:52:34 +1000

    hmmm

--- app/layout.jsx
+++ app/layout.jsx
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-TQ296WXD');
+            })(window,document,'script','dataLayer','GTM-WKRWHJDT');
           `}
                 </Script>
                 {/* End Google Tag Manager */}
@@ -65,7 +65,7 @@ export default function RootLayout({ children }) {
                 {/* GTM NoScript */}
                 <noscript>
                     <iframe
-                        src="https://www.googletagmanager.com/ns.html?id=GTM-TQ296WXD"
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-WKRWHJDT"
                         height="0"
                         width="0"
                         style={{ display: "none", visibility: "hidden" }}
